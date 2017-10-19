b0d00320103f8306a7b565eaf1486d53162ecae6
Oleg Pavlichenkov <=>
2017-10-19 16:00:28 +0300

    initial commit

---
+++ ntr/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ntr
+//
+//  Created by Oleg Pavlichenkov on 19/10/2017.
+//  Copyright © 2017 Oleg Pavlichenkov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
